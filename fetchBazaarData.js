e08cb157bbd8d197adc32b1bb1b38b764d19be83
KingOfHell91 <leo.kroha@icloud.com>
2025-02-05 10:34:57 +0100

    Delete fetchBazaarData.js

--- fetchBazaarData.js
+++
@@ -1,5 +0,0 @@
-export async function fetchBazaarData() {
-    const response = await fetch("https://api.hypixel.net/skyblock/bazaar");
-    const data = await response.json();
-    return data.products;
-}
